3ca12c936518d110e68d1cc237b208c41e26cb1a
juhairulislam <juhairulislam2018@gmail.com>
2026-05-10 08:12:13 +0600

    add three navlink in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
+        <nav>
+
+            <ul>
+                <li><Link href={'/'}>Home</Link></li>
+                <li><Link href={'/destinations'}>Destinations</Link></li>
+                <li><Link href={'/my-bookings'}>My Bookings</Link></li>
+            </ul>
             
-        </div>
+        </nav>
     );
 };
 
