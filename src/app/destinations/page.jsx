52304709eecc7d9e41c229d88e06dd96729bfb14
juhairulislam <juhairulislam2018@gmail.com>
2026-05-10 14:27:38 +0600

    show data from backend

--- src/app/destinations/page.jsx
+++ src/app/destinations/page.jsx
@@ -3,12 +3,24 @@ import React from 'react';
 const DestinationPage =async () => {
 
     const res =await fetch('http://localhost:5000/destination') ;
-    const destination = await res.json() ;
+    const destinations = await res.json() ;
 
-    console.log(destination)
+    // console.log(destinations)
 
     return (
         <div>
+            <h1>All Destinations</h1>
+
+
+            <div>
+
+                {
+                    destinations.map(d => <div key={d._id}>
+                        {d.destinationName}
+                    </div>)
+                }
+
+            </div>
         </div>
     );
 };
